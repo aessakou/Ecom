6ffe5b6a6924bee120bd5260c2e519c6e0604e14
aessakou <aessakou@github.com>
2024-05-16 03:02:54 +0100

    Login and logout from admin profile (logout X )

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 
 urlpatterns = [
 	path('', views.adminLogin, name='admin_login'),
+	path('logout/', views.logout, name='logout'),
+	path('', LogoutView.as_view(template_name='users/admin_login.html'), name='logoutsure'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.contrib
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def adminLogin(request):
+	if request.method == 'POST':
+		print('####>>>>>>FROM LOGIN FORM<<<<<####')
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'You are login')
+			return redirect('dashboard')
+		else:
+			messages.warning(request, 'Wrong username or password')
+	else:
+		print('####>>>>>>ITS NOT POST METHOD<<<<<####')
+
 	context = {
 		'title': 'Admin Login',
 	}
 	return render(request, 'users/admin_login.html', context)
+
+
+def logout(request):
+	context = {
+		'title': 'Logout',
+	}
+	return render(request, 'users/logout.html', context)
