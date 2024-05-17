65fe378b014f8f7a47f641ee27588f0289bbc978
aessakou <aessakou@github.com>
2024-05-17 22:56:59 +0100

    Setup cart and checkout templates, and fixing navbar (cart icon...)

--- src/store/views.py
+++ src/store/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def store(request):
 	return render(request, 'store/home.html', {'title': 'store',})
+
+def cart(request):
+	return render(request, 'store/cart.html', {'title': 'cart',})
+
+def checkout(request):
+	return render(request, 'store/checkout.html', {'title': 'checkout',})
