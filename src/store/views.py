d09c78e1ed81e987d53bdf32518ce9bfb7230714
aessakou <aessakou@github.com>
2024-05-19 22:25:40 +0100

    Add models to database and rendering Products

--- src/store/views.py
+++ src/store/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def store(request):
-	return render(request, 'store/home.html', {'title': 'store',})
+	products = Product.objects.all()
+	context = {
+		'title': 'store',
+		'products': products,
+	}
+	return render(request, 'store/home.html', context)
 
 def cart(request):
 	return render(request, 'store/cart.html', {'title': 'cart',})
