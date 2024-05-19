63836326a554916e2142c9030988d3a62890603f
aessakou <aessakou@github.com>
2024-05-19 02:14:18 +0100

    Add Checkout page (Order summary and Shipping Information form)

--- src/ecom/settings.py
+++ src/ecom/settings.py
@@ -130,3 +130,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+ALLOWED_HOSTS = [
+	"172.23.164.81",
+	"127.0.0.1"
+]
