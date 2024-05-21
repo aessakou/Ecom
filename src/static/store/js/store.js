b3481c8d7b7cdb98f1521cd51e717f9487a316c7
aessakou <aessakou@github.com>
2024-05-21 21:24:40 +0100

    New store card style (Responsive)

--- src/static/store/js/store.js
+++ src/static/store/js/store.js
@@ -1,15 +1,20 @@
-function image_height(){
-	console.log('hello');
-	const productImgs = document.querySelectorAll('.summary-img');
-	productImgs.forEach(item=>{
-			let width = item.offsetWidth;
-			let height = (9 * width) / 16;
-			item.style.height = height + 'px';
-			item.style.width = width + 'px';
-			console.log(height);
-		});
+// function image_height(){
+// 	console.log('hello');
+// 	const productImgs = document.querySelectorAll('.summary-img');
+// 	productImgs.forEach(item=>{
+// 			let width = item.offsetWidth;
+// 			let height = (9 * width) / 16;
+// 			item.style.height = height + 'px';
+// 			item.style.width = width + 'px';
+// 			if (width < item.parentElement.offsetWidth - 5)
+// 			{
+// 				item.style.height = item.parentElement.offsetHeight + 'px';
+// 				item.style.width = item.parentElement.offsetWidth + 'px';
+// 			}
 
-}
+// 		});
+
+// }
 
 
 // image_height();
