8de353f197c36eb874e9c682e48c0993936e1018
aessakou <aessakou@github.com>
2024-06-09 21:45:13 +0100

    Fixing create product form

--- src/static/store/js/magazine.js
+++ src/static/store/js/magazine.js
@@ -2,14 +2,66 @@
 
 addEventListener('DOMContentLoaded', (e)=>{
 	const listImgs = document.querySelectorAll('.product-list img');
-	listImgs.forEach((item, index)=>{
-		item.addEventListener('mouseover', (ev)=>{
+	if (listImgs) {
+		listImgs.forEach((item, index)=>{
+			item.addEventListener('mouseover', (ev)=>{
+			});
 		});
-	});
+	}
 
 	const addProduct = document.getElementById('add-product-id');
 	
-	addProduct.addEventListener('click', function(){
-		console.log('ADD PRODUCT');
-	});
+	if (addProduct) {
+		addProduct.addEventListener('click', function(){
+			console.log('ADD PRODUCT');
+		});
+	}
+
+	const uploadBaseImg = document.querySelector('.create-product-box .baseimg');
+
+	if (uploadBaseImg) {
+		const baseImg = document.querySelector('.create-product-box .baseimg img');
+		const fileInput = document.getElementById('base-img-id');
+		uploadBaseImg.addEventListener('click', function(){
+			console.log('Uplaod base image');
+			fileInput.click();
+		});
+
+		fileInput.addEventListener('change', function (event) {
+			const file = event.target.files[0];
+			if (file) {
+				const reader = new FileReader();
+				reader.onload = function (e) {
+					baseImg.src = e.target.result;
+					const styleElement = document.createElement('style');
+					styleElement.textContent = `
+						.create-product-box .baseimg:hover a::before {
+							content: "Change the image";
+							min-width: 125px;
+							left: -127px;
+						}
+					`;
+					document.head.appendChild(styleElement);
+				}
+				reader.readAsDataURL(file);
+			}
+		});
+	}
+
+	const isDigital = document.getElementById('is_digital');
+
+	if (isDigital) {
+		isDigital.addEventListener('change', function(e){
+			e.preventDefault();
+			const productForm = document.getElementById('createpro-form-id');
+			if (productForm.digital.value == 'false') {
+				productForm.shprice.classList.remove('d-none');
+				productForm.shcities.classList.remove('d-none');
+			}
+			else {
+				productForm.shprice.classList.add('d-none');
+				productForm.shcities.classList.add('d-none');
+			}
+		});
+	}
 });
